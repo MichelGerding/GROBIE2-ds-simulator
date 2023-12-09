90813486a2d1d44a67708599489903305d6a4294
Michel Gerding <michelgerding@gmail.com>
2023-12-09 12:44:14 +0100

    created simple propogation.
    next to propogation i also did some reformatting by moving the graph to its own file

---
+++ libs/network/NetworkGraph.py
@@ -0,0 +1,79 @@
+from libs.node.nodes.NetworkNode import NetworkNode
+
+from matplotlib import pyplot as plt
+
+import networkx as nx
+
+import matplotlib
+
+
+
+class NetworkGraph:
+    """ This is a graph of the network.
+        It is used to keep track of the nodes and their connections.
+        this uses the networkx library to create a graph. """
+
+    def __init__(self):
+        self.graph = nx.DiGraph()
+
+    def add_node(self, node: NetworkNode):
+        """ add a node to the graph. this will also recompute the neighbours of the nodes."""
+        self.graph.add_node(node)
+        # get all nodes that are within range of the new node
+        for n in self.graph.nodes:
+            self.update_neighbours(n)
+
+    def update_neighbours(self, node: NetworkNode):
+        """ update the neighbours of the node """
+        # get all nodes that are within range of the new node
+        nodes = list(self.graph.nodes)  # Create a copy of the nodes
+
+        # loop over all nodes and check if they need to update their neighbours
+        for n in nodes:
+            if n.distance(node) <= n.r and n != node:
+                self.graph.add_edge(n, node, weight=n.distance(node))
+
+    def remove_node(self, node: NetworkNode):
+        """ remove a node from the graph."""
+        self.graph.remove_node(node.node_id)
+
+    def get_neighbours(self, node: NetworkNode):
+        """ get all neighbours of the node """
+        return list(self.graph.neighbors(node))
+
+    def draw(self, file_name=None):
+        """ draw the graph with the actual positions and ranges.
+            if given a file_name it will save the graph to a file. """
+
+        # Create dictionaries with the positions and labels of the nodes
+        pos = {node: (node.x, node.y) for node in self.graph.nodes}
+        labels = {node: f"{node.node_id}\nRadius: {node.r}" for node in self.graph.nodes}
+
+        # Create a colormap based on the range values
+        cmap = matplotlib.colormaps['viridis']
+        norm = plt.Normalize(vmin=min(node.r for node in self.graph.nodes),
+                             vmax=max(node.r for node in self.graph.nodes))
+
+        # Create the figure and axes
+        fig, ax = plt.subplots(figsize=(8, 8))
+
+        # Draw nodes with colors based on the radius
+        nx.draw_networkx_nodes(self.graph, pos=pos, node_size=300,
+                               node_color=[cmap(norm(node.r)) for node in self.graph.nodes], ax=ax)
+        nx.draw_networkx_edges(self.graph, pos=pos, ax=ax)
+        nx.draw_networkx_labels(self.graph, pos, labels, font_size=8, font_color="black", ax=ax)
+
+        # Draw circles to represent node radius with the color based on the node range
+        for node, (x, y) in pos.items():
+            circle = plt.Circle((x, y), node.r, color=cmap(norm(node.r)), fill=False)
+            ax.add_patch(circle)
+
+        # Set the title and make the layout tight
+        ax.set_title("Network topology")
+        fig.tight_layout()
+
+        # Save the figure to a file or show it
+        if file_name:
+            fig.savefig(file_name)
+        else:
+            plt.show()
--- libs/node/nodes/NetworkNode.py
+++ libs/node/nodes/NetworkNode.py
@@ -1,9 +1,11 @@
-from abc import ABC, abstractmethod
-
 from libs.network.Message import Message
 
+from abc import ABC, abstractmethod
 
 class NetworkNode(ABC):
+
+    received_messages: list[str]
+    messages_send_counter: int
 
     def __init__(self, network, node_id, config, x, y, r):
         self.network = network
@@ -16,17 +18,30 @@ class NetworkNode(ABC):
 
         network.join_network(self)
 
+        self.received_messages = []
+        self.messages_send_counter = 0
+
     def distance(self, node):
         return ((self.x - node.x) ** 2 + (self.y - node.y) ** 2) ** 0.5
 
     def rec_message(self, message: Message):
         """ handle messages that are received """
-        # if it is our own message we can ignore it
-        if message.sending_id == self.node_id:
+        # if we have already received this message or it is our own we can ignore it
+        if message.msg_id in self.received_messages or message.sending_id == self.node_id:
             return
 
-        # TODO:: propagate the message if not for us.
-        self.handle_message(message)
+        self.received_messages.append(message.msg_id)
+
+        if message.receiving_id != self.node_id:
+            self.propagate_message(message)
+
+        if message.receiving_id == self.node_id or message.receiving_id == 0xFF:
+            self.handle_message(message)
+
+    def propagate_message(self, message: Message):
+        # TODO:: implement algorithm to propagate messages
+        message.hops += 1
+        self.send_message(message.receiving_id, message.message, message.channel)
 
     @abstractmethod
     def handle_message(self, message: Message):
@@ -42,5 +57,8 @@ class NetworkNode(ABC):
             message=message,
             sending_id=self.node_id,
             receiving_id=receiving_id,
-            channel=channel
+            channel=channel,
+            msg_id=f'{self.node_id}|{self.messages_send_counter}',
         ), self)
+
+        self.messages_send_counter += 1
